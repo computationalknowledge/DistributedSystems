8a2b568a7c03b0ff42261820754953a1781e0cbe
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2022-10-06 21:29:56 -0400

    Create File1.js

---
+++ File1.js
@@ -0,0 +1,19 @@
+// we need 2 classes and 2 objects
+// we need to connect the classes by method calls
+// make an example of shared field composition
+
+public class Example1{
+	public static void main(String[] args){
+		Student s1 = new Student();
+		ClassSession classSession;
+	}
+}
+
+class Student{
+	public String Studentname;
+}
+
+class ClassSession{
+	public String ClassID;
+}
+
